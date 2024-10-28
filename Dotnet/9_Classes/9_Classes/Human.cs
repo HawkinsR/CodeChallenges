bf36d72db21ddbbdad15e9a26cf297d0fd26d4bd
Richard Hawkins <Richard.Hawkins@revature.com>
2024-10-28 17:34:26 -0400

    update challenges to dotnet 8

---
+++ Dotnet/9_Classes/9_Classes/Human.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("9_ClassesChallenge.Tests")]
+namespace _9_ClassesChallenge
+{
+    internal class Human
+    {
+        /*
+        *
+        * implement the required class here.
+        *
+        */
+    }//end of class
+}//end of namespace
